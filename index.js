8f84b7b250d6a2a72154398b69a5c62b560b71d3
Paudelkritagya <paudelkritagya027@gmail.com>
2023-02-02 19:24:49 +0545

    Connected the application to a firebase database

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
+
 // required dom elements
 const buttonEl = document.getElementById('button');
 const messageEl = document.getElementById('message');
 const titleEl = document.getElementById('real-time-title');
+
 
 // initial states and global variables
 messageEl.style.display = 'none';
 let isRecording = false;
 let socket;
 let recorder;
-let arduinoSocket;
+//let arduinoSocket;
+
+// Initialize the firebase database
+var firebaseConfig = {
+    apiKey: "AIzaSyCFnvtdTVkQZzfvhzOukWH7RpzY7ioen8Y",
+    authDomain: "esp32-fb-dfd3a.firebaseapp.com",
+    databaseURL: "https://esp32-fb-dfd3a-default-rtdb.firebaseio.com",
+    projectId: "esp32-fb-dfd3a",
+    storageBucket: "esp32-fb-dfd3a.appspot.com",
+    messagingSenderId: "543332848862",
+    appId: "1:543332848862:web:c0d022cb4c1667f2241e4f",
+    measurementId: "G-RH4LQTG4GN"
+};
+
+const firebase = initializeApp(firebaseConfig);
+
 
 const run = async () => {
     isRecording = !isRecording;
@@ -42,13 +59,15 @@ const run = async () => {
 
         // establish wss with AssemblyAI at 16000 sample rate
         socket = new WebSocket(`wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000&token=${token}`);
-        
-        arduinoSocket = new WebSocket('ws://172.20.10.3');
+        // arduinoSocket = new WebSocket('wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self');
+
+        //arduinoSocket = new WebSocket('ws://192.168.1.71');
 
 
         // handle incoming messages to display transcription to the DOM
         const texts = {};
         socket.onmessage = (message) => {
+            console.log("msg received")
             let msg = '';
             const res = JSON.parse(message.data);
             texts[res.audio_start] = res.text;
@@ -61,11 +80,25 @@ const run = async () => {
             }
             messageEl.innerText = msg;
 
+            var database = firebase.database();
 
-            if (arduinoSocket){
-                console.log("connection with arduinoSocket established");
-                arduinoSocket.send(JSON.stringify(msg));
-            }
+            // Send a message to the database
+            var messageRef = database.ref("message");
+            messageRef.set("Hello World!");
+
+            // socketio.emit('data', { data: 'hello from JavaScript' });
+            // arduinoSocket.onopen = () => arduinoSocket.send(JSON.stringify(msg));
+            // arduinoSocket.onopen = function() {
+                // console.log("Connected to the websocket server");
+            // Send data to the websocket server
+                // arduinoSocket.send(JSON.stringify(msg));
+            // };
+            // arduinoSocket.send(JSON.stringify(msg));
+
+            // if (arduinoSocket){
+            //     console.log("connection with arduinoSocket established");
+            //     arduinoSocket.send(JSON.stringify(msg));
+            // }
         };
 
 
